6fb718f881a6e775e650db65c31ec4fa63972c4f
himanshu soni <himanshusoninagod@gmail.com>
2025-06-26 17:31:42 +0530

    updated code

---
+++ june 24 (Object)/index.js
@@ -0,0 +1,73 @@
+// Object-- unordered{}
+// value in key value
+
+// let ob={
+//     name: "Himanshu",
+//     contact: 9589152996,
+//     city: "Bhopal" 
+// }
+// console.log(ob)
+// console.log(ob.name)
+// console.log(ob.city)
+// console.log(ob.contact)
+
+
+// let ob={
+//     name: "",
+//     contact: "",
+//     city: "" 
+// }
+
+// ob.name=prompt("Enter name: ")
+// ob.contact=parseInt(prompt("Enter contact: "))
+// ob.city=prompt("Enter city: ")
+
+// console.log(ob.name)
+// console.log(ob.city)
+// console.log(ob.contact)
+
+// or 
+// let ob={
+//     name:prompt("Enter name: ") ,
+//     contact:parseInt(prompt("Enter contact: ")),
+//     city:prompt("Enter city: ") 
+// }
+
+// console.log(ob.name)
+// console.log(ob.city)
+// console.log(ob.contact)
+
+// let emp={
+//     emp_name:"Himanshu",
+//     emp_id:101,
+//     emp_desgination:"SDE",
+//     emp_contact:[9589152996,9589897133],
+//     emp_address:["Surendra palace","Narayan Nagar"]
+// }
+
+// console.log(emp.emp_contact[0])
+// console.log(emp.emp_contact[1])
+// console.log(emp.emp_address[0])
+// console.log(emp.emp_address[1])
+
+// adding array value in object --
+// emp.emp_contact.push(7566332274)
+// console.log(emp.emp_contact)
+
+let emp={
+    emp_name:"Himanshu",
+    emp_id:101,
+    emp_desgination:"SDE",
+    emp_contact:[9589152996,9589897133],
+    emp_address:["Surendra palace","Narayan Nagar"]
+}
+emp.emp_mail= "himanshusoninagod@gmail.com"
+// console.log(emp.emp_mail)
+
+for(let k in emp){
+    console.log(emp[k])
+}
+
+// print only one key 1
+
+
---
+++ june 25 (Function)/index.js
@@ -0,0 +1,136 @@
+// function add(){
+//     console.log("Hello")
+// }
+// add()
+
+
+// write a program print a table of any number using fun?
+
+// function table(){
+//     let a=5
+//     for(let i=1; i<=10; i++){
+//         console.log(a*i)
+//     }
+// }
+// table()
+
+// function- User defined function, Pre defined function
+// arguement -- as a input
+// there are 4 types of user defined function-
+// 1. without arguement and no return value
+// 2. with arguement and no return value
+// 3. with arguement and with return value
+// 4. without arguement and with return value
+
+
+// function type1(a){
+//     return a+a;
+// }
+// let b=8
+// let c=type1(b)
+// console.log(type1(b))
+ 
+
+//1. without arguement and no return value 
+// function cloud(a){
+// console.log(a*a*a)
+// }
+// cloud(10)
+
+
+//2. with arguement and no return value 
+// write a program to check no is positive or negetive using function type 2?
+// function check(b){
+//     if (b>0){
+//         console.log("Even")
+//     }
+//     else{
+//         console.log("Odd")
+//     }
+// }
+// let a=parseInt(prompt("Enter a number: "))
+// check(a)
+
+
+//3. with arguement and return value 
+// function demo(a){
+//     return a+a;
+// }
+// demo(10)
+// console.log(demo(10))
+
+// let b=demo(10)
+// console.log(3.14*b*b)
+
+
+//4. without arguement and return value 
+// function demo(){
+//     let a=prompt("Enter your name: ")
+//     return a;
+// }
+// demo()
+// console.log(demo())
+
+
+// H.W. ---
+// what is ES6?
+
+// arrow function--
+
+// 1. () => {}   return 
+// 2. () => ()   
+// 3. () => 
+// 4.    => 
+
+// let fun = () => {return console.log("Hii")}
+// fun()
+
+
+// let demo = (a) => {return a+a}
+// console.log(demo(2))
+// or 
+// let demo = (a,b) => {return a+b}
+// console.log(demo(5,7))
+
+
+// ======Object======
+
+// when we have to print return value, we shoud write console.log 
+// let ob={
+//     name:"himanshu",
+//     age:24,
+//     phone:9589152996,
+//     address:"Bhopal",
+//     location: function(){
+//         alert("MP Nagar")
+//     },
+//     country : () => {return "India"}
+// }
+// ob.location()
+// console.log(ob.country())
+
+
+// write a program to take three inputs from the user and print multiplication of 3 using type 3 function?
+// with AggregateError.and return value?
+// let a=parseInt(prompt("Enter value of a: "))
+// let b=parseInt(prompt("Enter value of b: "))
+// let c=parseInt(prompt("Enter value of c: "))
+
+// function mult(d,e,f){
+//     return d*e*f
+// }
+// console.log(mult(a,b,c))
+
+
+// write a program to take radious from the user and print the area of circle?
+// using without arg. and return value 
+
+// function radious(){
+//     let a=parseInt(prompt("Enter radious: "))
+//     return 3.14*a*a
+// }
+// console.log(radious())
+
+
+
+
